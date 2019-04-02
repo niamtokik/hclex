25a0074b54986be5c309520e960f0b63ecc7c386
niamtokik <niamtokik@localhost>
2019-04-02 15:52:49 +0000

    updated license, create lexer and first tests.

--- test/hclex_test.exs
+++ test/hclex_test.exs
@@ -1,8 +1,33 @@
-defmodule HclexTest do
-  use ExUnit.Case
-  doctest Hclex
+defmodule Hclex.LexerTest do
+  use ExUnit.Case, async: true
+  
+  test "simple identifier" do
+    str = "test"
+    ret = [{:identifier, "test"}]
+    assert Hclex.Lexer.lex(str), ret
+  end
 
-  test "greets the world" do
-    assert Hclex.hello() == :world
+  test "simple string" do
+    str = "\"this is a string\""
+    ret = [{:string, "this is a string"}]
+    assert Hclex.Lexer.lex(str), ret
+  end
+
+  test "simple number" do
+    str = "1234"
+    ret = [{:number, 1234}]
+    assert Hclex.Lexer.lex(str), ret
+  end
+
+  test "simple attributes" do
+    str = "identifier = 1234"
+    ret = [{:identifier, "identifier"}, :equal, {:number, 1234}]
+    assert Hclex.Lexer.lex(str), ret
+  end
+
+  test "simple block" do
+    str = ""
+    ret = ""
+    assert Hclex.Lexer.lex(str), ret
   end
 end
